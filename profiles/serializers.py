588e11c2b064961caaf2b75b19b357b96859a9e6
Giulio Montaque <giuliom@hotmail.co.uk>
2024-07-17 17:28:58 +0000

    update

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,6 +5,9 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    post_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self,obj):
         request = self.context['request']
@@ -13,5 +16,6 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = [
-            'id','owner','created_at', 'updated_at', 'name', 'content', 'image', 'is_owner'
+            'id','owner','created_at', 'updated_at', 'name', 'content', 'image', 'is_owner',
+            'post_count','followers_count','following_count'
         ]
