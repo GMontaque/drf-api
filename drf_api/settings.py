15f73a4ab6911e35d5b32670ba4fa31bd1008b3e
Giulio Montaque <giuliom@hotmail.co.uk>
2024-07-26 16:52:26 +0000

    update

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -34,6 +35,11 @@ REST_FRAMEWORK = {
 
 if "CLIENT_ORIGIN" in os.environ:
     CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV',''), re.IGNORECASE).group(0)
+ 
+    CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.codeinstitute-ide\.net$",]
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGIN_REGEXES = [
