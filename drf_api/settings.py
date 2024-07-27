4a9993bfb9d1d1825fe163954aae52304e085dc2
Giulio Montaque <giuliom@hotmail.co.uk>
2024-07-27 07:18:30 +0000

    update

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -39,8 +39,11 @@ if "CLIENT_ORIGIN" in os.environ:
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV',''), re.IGNORECASE).group(0)
  
-    CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.codeinstitute-ide\.net$",]
-
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.codeinstitute-ide\.net$",
+        r"^http:\/\/localhost:*([0-9]+)?$",
+        r"^https:\/\/localhost:*([0-9]+)?$"
+    ]
 CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOWED_ORIGIN_REGEXES = [
 #     r"^https://.*\.codeinstitute-ide\.net$",
